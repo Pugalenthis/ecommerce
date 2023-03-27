8283ae04a7b85a9f096489e4aadfcbb7871e93b4
Pugalenthi <pugalenthi@trdfin.in>
2023-03-27 21:28:53 +0530

    backend authentication done

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.header.access_token;
+  if (!token) {
+    return next(createError(401, "You are not authenticated!"));
+  }
+  jwt.verify(token, "jwt", (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+    req.user = user;
+    next();
+  });
+};
+
+export const verifyUser = (req, res, next) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    next();
+  } else {
+    return next(createError(403, "You are not authorized!"));
+  }
+};
+
+export const verifyAdmin = (req, res, next) => {
+  if (req.user.isAdmin) {
+    next();
+  } else {
+    return next(createError(403, "You are not authorized!"));
+  }
+};
---
+++ routes/users.js
@@ -0,0 +1,192 @@
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+const router = express.Router();
+import jwt from "jsonwebtoken";
+import {
+  verifyToken,
+  verifyAdmin,
+  verifyUser,
+} from "../middlewares/authMiddleware.js";
+import { check, validationResult } from "express-validator";
+import { createError } from "../../utils/error.js";
+// import nodemailer from "nodemailer";
+// import Handlebars from "handlebars";
+import fs from "fs";
+
+// @route   POST api/user/register
+// @desc    register user
+// @access  public
+
+router.post(
+  "/register",
+  check("name", "Name is required").notEmpty(),
+  check("email", "Please include a valid email").isEmail(),
+  check(
+    "password",
+    "Please enter a password with 6 or more characters"
+  ).isLength({ min: 6 }),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(createError(500, { errors: errors.array() }));
+    }
+    const { email, name, password } = req.body;
+    try {
+      // check user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return next(
+          createError(400, { errors: [{ msg: "User already exists" }] })
+        );
+      }
+
+      //hash password
+      var salt = bcrypt.genSaltSync(10);
+      var hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+      user = new User({ email, name, password: hashedPassword });
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+          isAdmin: user.isAdmin,
+        },
+      };
+
+      // generate and return jwt
+      jwt.sign(payload, process.env.JWT, (err, token) => {
+        if (err) throw err;
+        res
+          .cookie("access_token", token, {
+            httpOnly: true,
+          })
+          .status(200)
+          .json({ token });
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// @route   POST api/user/login
+// @desc    login route
+// @access  public
+
+router.post(
+  "/login",
+
+  check("email", "Please include a valid email").isEmail(),
+  check("password", "Password is required").exists(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(createError(500, { errors: errors.array() }));
+    }
+
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return next(
+          createError(400, { errors: [{ msg: "Invalid Credentials" }] })
+        );
+      }
+
+      const isMatch = await bcrypt.compare(req.body.password, user.password);
+      console.log("isMatch", isMatch);
+      if (!isMatch)
+        return next(
+          createError(400, { errors: [{ msg: "Invalid Credentials" }] })
+        );
+      const payload = {
+        user: {
+          id: user.id,
+          isAdmin: user.isAdmin,
+        },
+      };
+
+      // generate and return jwt
+      jwt.sign(payload, process.env.JWT, (err, token) => {
+        if (err) throw err;
+        res
+          .cookie("access_token", token, {
+            httpOnly: true,
+          })
+          .status(200)
+          .json({ token });
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// router.post("/forgot-password/:email", async (req, res, next) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.AUTH_EMAIL,
+//         pass: process.env.AUTH_PASS,
+//       },
+//     });
+
+//     var readHTMLFile = function (path, callback) {
+//       fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
+//         if (err) {
+//           callback(err);
+//         } else {
+//           callback(null, html);
+//         }
+//       });
+//     };
+
+//     readHTMLFile("api/user/routes/forgot-password.html", function (err, html) {
+//       if (err) {
+//         console.log("error reading file", err);
+//         return;
+//       }
+//       var template = Handlebars.compile(html);
+//       var replacements = {
+//         email: req.params.email,
+//         urlLink: `http://localhost:3000/reset-password/${req.params.email}`,
+//       };
+//       var htmlToSend = template(replacements);
+//       var mailConfigurations = {
+//         from: process.env.AUTH_EMAIL,
+//         to: req.params.email,
+//         subject: "Reset Password Link",
+//         html: htmlToSend,
+//       };
+//       transporter.sendMail(mailConfigurations, function (error, info) {
+//         if (error) throw Error(error);
+//         console.log("Email Sent Successfully");
+//         console.log(info);
+//       });
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// router.put("/reset-password", async (req, res, next) => {
+//   try {
+//     //hash password
+//     var salt = bcrypt.genSaltSync(10);
+//     var hashedPassword = bcrypt.hashSync(req.body.password, salt);
+//     const user = await User.findOneAndUpdate(
+//       { email: req.body.email },
+//       {
+//         $set: { password: hashedPassword },
+//       }
+//     );
+//     res.status(200).json("password reset successfully");
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+export default router;
